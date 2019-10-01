d6d9d23d12b2062bac6df6302337ef78cf721ce5
Claudio Mendonca <claudioccm@gmail.com>
2019-10-01 15:01:52 -0700

    modal-dummy

---
+++ source/_patterns/molecules/blocks/modal/modal.js
@@ -0,0 +1,9 @@
+require('a11y-dialog')
+
+var dialog = new window.A11yDialog(dialogEl, mainEl);
+
+// // Get the dialog element (with the accessor method you want)
+// const el = document.getElementById('my-accessible-dialog');
+
+// // Instantiate a new A11yDialog module
+// const dialog = new A11yDialog(el);
