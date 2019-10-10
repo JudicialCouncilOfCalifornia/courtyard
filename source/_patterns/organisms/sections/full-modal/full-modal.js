95e0da4af01b5ab44912ae3bdcb8c687c318a013
Claudio Mendonca <claudioccm@gmail.com>
2019-10-10 11:21:09 -0300

    incorporating js to the modal

---
+++ source/_patterns/organisms/sections/full-modal/full-modal.js
@@ -0,0 +1,7 @@
+import A11yDialog from "a11y-dialog";
+
+// Get the dialog element (with the accessor method you want)
+const el = document.getElementById("jcc-full-modal");
+
+// Instantiate a new A11yDialog module
+const dialog = new A11yDialog(el);
