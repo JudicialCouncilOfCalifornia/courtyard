05e4134f861e8d46d0f555499f24662098b42a37
Jesse Arnold <jessearnold@Jesses-MacBook-Pro.local>
2019-10-15 15:12:24 -0700

    Demo layouts

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -64,7 +64,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(51);
+  var r = n(50);
 
   function i(e, t, n, r) {
     var i = e(r, n),
@@ -168,11 +168,11 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(36),
+  var r = n(35),
       i = n(3),
-      o = n(37),
-      a = n(38),
-      s = n(39),
+      o = n(36),
+      a = n(37),
+      s = n(38),
       u = n(10);
 
   function l(e, t, n) {
@@ -244,9 +244,9 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
 
-  var r = n(40),
-      i = n(41),
-      o = n(42),
+  var r = n(39),
+      i = n(40),
+      o = n(41),
       a = function a() {
     this.callbacks = new i["default"](), this.classNames = new o["default"](), this.behavior = new r["default"](), Object.seal(this);
   };
@@ -4283,6 +4283,14 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     }, i || (n.jQuery = n.$ = E), E;
   });
 }, function (e, t, n) {
+  "use strict";
+
+  Object.defineProperty(t, "__esModule", {
+    value: !0
+  }), n(31), n(32);
+  var r = n(33);
+  t["default"] = r["default"];
+}, function (e, t, n) {
   (function (n) {
     var r;
     !function (n) {
@@ -4366,15 +4374,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     }(void 0 !== n || window);
   }).call(this, n(7));
 }, function (e, t, n) {
-  "use strict";
-
-  Object.defineProperty(t, "__esModule", {
-    value: !0
-  }), n(32), n(33);
-  var r = n(34);
-  t["default"] = r["default"];
-}, function (e, t, n) {
-  n(25), n(27), n(28), n(29), n(31), n(84), n(86), e.exports = n(87);
+  n(25), n(27), n(28), n(30), n(83), n(85), n(86), e.exports = n(87);
 }, function (e, t, n) {
   (function (e) {
     var t, r, i;
@@ -5762,15 +5762,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   "use strict";
 
   n.r(t);
-  var r = n(22),
-      i = n.n(r);
-  var o = document.getElementById("jcc-full-modal");
-  new i.a(o);
-}, function (e, t, n) {
-  "use strict";
-
-  n.r(t);
-  n(30);
+  n(29);
 }, function (e, t) {
   /*!
    * @copyright Copyright (c) 2017 IcoMoon.io
@@ -5881,7 +5873,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   "use strict";
 
   n.r(t);
-  var r = n(23);
+  var r = n(22);
   n.n(r).a.all();
 }, function (e, t) {
   !function () {
@@ -5924,8 +5916,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
   var r = n(8),
-      i = n(35),
-      o = n(83);
+      i = n(34),
+      o = n(82);
 
   function a(e, t) {
     void 0 === t && (t = {});
@@ -5964,17 +5956,17 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
   var r = n(0),
       i = n(11),
-      o = n(43),
-      a = n(60),
+      o = n(42),
+      a = n(59),
       s = n(1),
-      u = n(73),
-      l = n(74),
-      c = n(75),
-      f = n(76),
-      d = n(77),
-      p = n(79),
+      u = n(72),
+      l = n(73),
+      c = n(74),
+      f = n(75),
+      d = n(76),
+      p = n(78),
       h = n(8),
-      v = n(82),
+      v = n(81),
       g = function () {
     function e(e, t) {
       this.config = r["default"](new i["default"](), t, !0), this.state = p["default"].mapFromSelect(e, this.config), this.renderer = new a["default"](this.config.classNames), this.dom = this.renderer.render(this.state, e), this.timers = new v["default"](), this.actions = d["default"].proxyActions(this.state, {
@@ -6187,9 +6179,9 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(44),
-      i = n(45),
-      o = n(46);
+  var r = n(43),
+      i = n(44),
+      o = n(45);
 
   function a(e, t) {
     var n = new i["default"](t);
@@ -6252,16 +6244,16 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(47),
-      i = n(48),
-      o = n(49),
-      a = n(50),
-      s = n(52),
-      u = n(53),
-      l = n(54),
-      c = n(55),
-      f = n(56),
-      d = n(59),
+  var r = n(46),
+      i = n(47),
+      o = n(48),
+      a = n(49),
+      s = n(51),
+      u = n(52),
+      l = n(53),
+      c = n(54),
+      f = n(55),
+      d = n(58),
       p = n(14),
       h = n(14);
 
@@ -6484,8 +6476,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   var r = n(1),
       i = n(6),
       o = n(13),
-      a = n(57),
-      s = n(58);
+      a = n(56),
+      s = n(57);
 
   t["default"] = function (e, t) {
     var n = e.keyCode,
@@ -6593,11 +6585,11 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
 
-  var r = n(61),
-      i = n(68),
-      o = n(69),
-      a = n(70),
-      s = n(72),
+  var r = n(60),
+      i = n(67),
+      o = n(68),
+      a = n(69),
+      s = n(71),
       u = function () {
     function e(e) {
       this.dom = new o["default"](), this.classNames = e;
@@ -6650,8 +6642,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
   var r = n(2),
-      i = n(62),
-      o = n(65);
+      i = n(61),
+      o = n(64);
 
   t["default"] = function (e, t) {
     return '\n        <div\n            class="' + r["default"]([t.root, [e.isDisabled, t.rootDisabled], [e.isInvalid, t.rootInvalid], [e.isOpen, t.rootOpen], [e.isFocused, t.rootFocused], [e.hasValue, t.rootHasValue], [e.isOpenAbove, t.rootOpenAbove], [e.isOpenBelow, t.rootOpenBelow], [e.isUseNativeMode, t.rootNative]]) + '"\n            role="widget combobox"\n            aria-haspopup="listbox"\n            ' + (e.isOpen ? 'aria-expanded="true"' : "") + "\n            " + (e.isRequired ? 'aria-required="true"' : "") + "\n            " + (e.isDisabled ? 'aria-disabled="true"' : "") + "\n            " + (e.isInvalid ? 'aria-invalid="true"' : "") + "\n        >\n            " + o["default"](e, t) + "\n            " + (e.isUseNativeMode ? "" : i["default"](e, t)) + "\n        </div>\n    ";
@@ -6663,7 +6655,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
   var r = n(2),
-      i = n(63);
+      i = n(62);
 
   t["default"] = function (e, t) {
     var n = r["default"]([t.body, [e.isAtTop, t.bodyAtTop], [e.isAtBottom, t.bodyAtBottom], [e.isScrollable, t.bodyScrollable]]),
@@ -6679,7 +6671,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
   var r = n(2),
-      i = n(64);
+      i = n(63);
 
   t["default"] = function (e, t, n) {
     return '\n        <div class="' + r["default"]([n.group, [e.isDisabled, n.groupDisabled], [e.hasLabel, n.groupHasLabel]]) + '" data-ref="group" role="group">\n            ' + (e.hasLabel ? '<div class="' + n.groupLabel + '" data-ref="item">' + e.label + "</div>" : "") + "\n            " + e.options.map(function (e) {
@@ -6704,8 +6696,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(66),
-      i = n(67);
+  var r = n(65),
+      i = n(66);
 
   t["default"] = function (e, t) {
     return '\n    <div class="' + t.head + '" data-ref="head">\n        ' + i["default"](e, t) + "\n        " + r["default"](e, t) + '\n        <select class="' + t.select + '" data-ref="select"></select>\n    </div>\n';
@@ -6772,7 +6764,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   var r = n(0),
       i = n(15),
       o = n(16),
-      a = n(71);
+      a = n(70);
 
   function s(e, t) {
     for (var n = Math.max(e.attributes.length, t.attributes.length), r = {}, a = [], s = 0; s < n; s++) {
@@ -6950,7 +6942,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   });
 
   var r = n(0),
-      i = n(78),
+      i = n(77),
       o = function () {
     function e() {}
 
@@ -7082,10 +7074,10 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   });
 
   var r = n(0),
-      i = n(80),
+      i = n(79),
       o = n(19),
       a = n(20),
-      s = n(81),
+      s = n(80),
       u = function () {
     function e() {}
 
@@ -7336,7 +7328,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
   t["default"] = r;
 }, function (e, t, n) {
-  n(85);
+  n(84);
 }, function (e, t) {
   !function () {
     "use strict";
@@ -7398,6 +7390,14 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     }), window && (window.a11yToggle = a);
   }();
 }, function (e, t) {}, function (e, t, n) {
+  "use strict";
+
+  n.r(t);
+  var r = n(23),
+      i = n.n(r);
+  var o = document.getElementById("jcc-full-modal");
+  new i.a(o);
+}, function (e, t, n) {
   (function (e) {
     n(88), e(function () {
       e("#slick-menu").slicknav({
