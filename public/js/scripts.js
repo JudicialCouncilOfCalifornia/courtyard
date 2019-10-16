072fb36c35baeb5605634dea1c4303c104aae5bb
Jesse Arnold <jessearnold@Jesses-MacBook-Pro.local>
2019-10-16 12:07:50 -0700

    Building out demo for onramp

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -64,7 +64,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(54);
+  var r = n(51);
 
   function i(e, t, n, r) {
     var i = e(r, n),
@@ -168,11 +168,11 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(39),
+  var r = n(36),
       i = n(3),
-      o = n(40),
-      a = n(41),
-      s = n(42),
+      o = n(37),
+      a = n(38),
+      s = n(39),
       u = n(10);
 
   function l(e, t, n) {
@@ -244,9 +244,9 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
 
-  var r = n(43),
-      i = n(44),
-      o = n(45),
+  var r = n(40),
+      i = n(41),
+      o = n(42),
       a = function a() {
     this.callbacks = new i["default"](), this.classNames = new o["default"](), this.behavior = new r["default"](), Object.seal(this);
   };
@@ -4370,11 +4370,11 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
   Object.defineProperty(t, "__esModule", {
     value: !0
-  }), n(35), n(36);
-  var r = n(37);
+  }), n(32), n(33);
+  var r = n(34);
   t["default"] = r["default"];
 }, function (e, t, n) {
-  n(25), n(27), n(28), n(29), n(30), n(32), n(34), e.exports = n(87);
+  n(25), n(27), n(28), n(29), n(31), n(84), n(86), e.exports = n(87);
 }, function (e, t, n) {
   (function (e) {
     var t, r, i;
@@ -5766,11 +5766,11 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
       i = n.n(r);
   var o = document.getElementById("jcc-full-modal");
   new i.a(o);
-}, function (e, t) {}, function (e, t, n) {
+}, function (e, t, n) {
   "use strict";
 
   n.r(t);
-  n(31);
+  n(30);
 }, function (e, t) {
   /*!
    * @copyright Copyright (c) 2017 IcoMoon.io
@@ -5878,68 +5878,6 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     }
   }();
 }, function (e, t, n) {
-  n(33);
-}, function (e, t) {
-  !function () {
-    "use strict";
-
-    var e = 0,
-        t = {},
-        n = {};
-
-    function r(e, t) {
-      return Array.prototype.slice.call((t || document).querySelectorAll(e));
-    }
-
-    function i(e) {
-      if (e.closest) return e.closest("[data-a11y-toggle]");
-
-      for (; e;) {
-        if (1 === e.nodeType && e.hasAttribute("data-a11y-toggle")) return e;
-        e = e.parentNode;
-      }
-
-      return null;
-    }
-
-    function o(e) {
-      var r = e && n[e.getAttribute("aria-controls")];
-      if (!r) return !1;
-      var i = t["#" + r.id],
-          o = "false" === r.getAttribute("aria-hidden");
-      r.setAttribute("aria-hidden", o), i.forEach(function (e) {
-        e.setAttribute("aria-expanded", !o);
-      });
-    }
-
-    var a = function a(i) {
-      t = r("[data-a11y-toggle]", i).reduce(function (e, t) {
-        var n = "#" + t.getAttribute("data-a11y-toggle");
-        return e[n] = e[n] || [], e[n].push(t), e;
-      }, t);
-      var o = Object.keys(t);
-      o.length && r(o).forEach(function (r) {
-        var i = t["#" + r.id],
-            o = r.hasAttribute("data-a11y-toggle-open"),
-            a = [];
-        i.forEach(function (t) {
-          t.id || t.setAttribute("id", "a11y-toggle-" + e++), t.setAttribute("aria-controls", r.id), t.setAttribute("aria-expanded", o), a.push(t.id);
-        }), r.setAttribute("aria-hidden", !o), r.hasAttribute("aria-labelledby") || r.setAttribute("aria-labelledby", a.join(" ")), n[r.id] = r;
-      });
-    };
-
-    document.addEventListener("DOMContentLoaded", function () {
-      a();
-    }), document.addEventListener("click", function (e) {
-      o(i(e.target));
-    }), document.addEventListener("keyup", function (e) {
-      if (13 === e.which || 32 === e.which) {
-        var t = i(e.target);
-        t && "button" === t.getAttribute("role") && o(t);
-      }
-    }), window && (window.a11yToggle = a);
-  }();
-}, function (e, t, n) {
   "use strict";
 
   n.r(t);
@@ -5986,8 +5924,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
   var r = n(8),
-      i = n(38),
-      o = n(86);
+      i = n(35),
+      o = n(83);
 
   function a(e, t) {
     void 0 === t && (t = {});
@@ -6026,17 +5964,17 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
   var r = n(0),
       i = n(11),
-      o = n(46),
-      a = n(63),
+      o = n(43),
+      a = n(60),
       s = n(1),
-      u = n(76),
-      l = n(77),
-      c = n(78),
-      f = n(79),
-      d = n(80),
-      p = n(82),
+      u = n(73),
+      l = n(74),
+      c = n(75),
+      f = n(76),
+      d = n(77),
+      p = n(79),
       h = n(8),
-      v = n(85),
+      v = n(82),
       g = function () {
     function e(e, t) {
       this.config = r["default"](new i["default"](), t, !0), this.state = p["default"].mapFromSelect(e, this.config), this.renderer = new a["default"](this.config.classNames), this.dom = this.renderer.render(this.state, e), this.timers = new v["default"](), this.actions = d["default"].proxyActions(this.state, {
@@ -6249,9 +6187,9 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(47),
-      i = n(48),
-      o = n(49);
+  var r = n(44),
+      i = n(45),
+      o = n(46);
 
   function a(e, t) {
     var n = new i["default"](t);
@@ -6314,16 +6252,16 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(50),
-      i = n(51),
-      o = n(52),
-      a = n(53),
-      s = n(55),
-      u = n(56),
-      l = n(57),
-      c = n(58),
-      f = n(59),
-      d = n(62),
+  var r = n(47),
+      i = n(48),
+      o = n(49),
+      a = n(50),
+      s = n(52),
+      u = n(53),
+      l = n(54),
+      c = n(55),
+      f = n(56),
+      d = n(59),
       p = n(14),
       h = n(14);
 
@@ -6546,8 +6484,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   var r = n(1),
       i = n(6),
       o = n(13),
-      a = n(60),
-      s = n(61);
+      a = n(57),
+      s = n(58);
 
   t["default"] = function (e, t) {
     var n = e.keyCode,
@@ -6655,11 +6593,11 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
 
-  var r = n(64),
-      i = n(71),
-      o = n(72),
-      a = n(73),
-      s = n(75),
+  var r = n(61),
+      i = n(68),
+      o = n(69),
+      a = n(70),
+      s = n(72),
       u = function () {
     function e(e) {
       this.dom = new o["default"](), this.classNames = e;
@@ -6712,8 +6650,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
   var r = n(2),
-      i = n(65),
-      o = n(68);
+      i = n(62),
+      o = n(65);
 
   t["default"] = function (e, t) {
     return '\n        <div\n            class="' + r["default"]([t.root, [e.isDisabled, t.rootDisabled], [e.isInvalid, t.rootInvalid], [e.isOpen, t.rootOpen], [e.isFocused, t.rootFocused], [e.hasValue, t.rootHasValue], [e.isOpenAbove, t.rootOpenAbove], [e.isOpenBelow, t.rootOpenBelow], [e.isUseNativeMode, t.rootNative]]) + '"\n            role="widget combobox"\n            aria-haspopup="listbox"\n            ' + (e.isOpen ? 'aria-expanded="true"' : "") + "\n            " + (e.isRequired ? 'aria-required="true"' : "") + "\n            " + (e.isDisabled ? 'aria-disabled="true"' : "") + "\n            " + (e.isInvalid ? 'aria-invalid="true"' : "") + "\n        >\n            " + o["default"](e, t) + "\n            " + (e.isUseNativeMode ? "" : i["default"](e, t)) + "\n        </div>\n    ";
@@ -6725,7 +6663,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
   var r = n(2),
-      i = n(66);
+      i = n(63);
 
   t["default"] = function (e, t) {
     var n = r["default"]([t.body, [e.isAtTop, t.bodyAtTop], [e.isAtBottom, t.bodyAtBottom], [e.isScrollable, t.bodyScrollable]]),
@@ -6741,7 +6679,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
     value: !0
   });
   var r = n(2),
-      i = n(67);
+      i = n(64);
 
   t["default"] = function (e, t, n) {
     return '\n        <div class="' + r["default"]([n.group, [e.isDisabled, n.groupDisabled], [e.hasLabel, n.groupHasLabel]]) + '" data-ref="group" role="group">\n            ' + (e.hasLabel ? '<div class="' + n.groupLabel + '" data-ref="item">' + e.label + "</div>" : "") + "\n            " + e.options.map(function (e) {
@@ -6766,8 +6704,8 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   Object.defineProperty(t, "__esModule", {
     value: !0
   });
-  var r = n(69),
-      i = n(70);
+  var r = n(66),
+      i = n(67);
 
   t["default"] = function (e, t) {
     return '\n    <div class="' + t.head + '" data-ref="head">\n        ' + i["default"](e, t) + "\n        " + r["default"](e, t) + '\n        <select class="' + t.select + '" data-ref="select"></select>\n    </div>\n';
@@ -6834,7 +6772,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   var r = n(0),
       i = n(15),
       o = n(16),
-      a = n(74);
+      a = n(71);
 
   function s(e, t) {
     for (var n = Math.max(e.attributes.length, t.attributes.length), r = {}, a = [], s = 0; s < n; s++) {
@@ -7012,7 +6950,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   });
 
   var r = n(0),
-      i = n(81),
+      i = n(78),
       o = function () {
     function e() {}
 
@@ -7144,10 +7082,10 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
   });
 
   var r = n(0),
-      i = n(83),
+      i = n(80),
       o = n(19),
       a = n(20),
-      s = n(84),
+      s = n(81),
       u = function () {
     function e() {}
 
@@ -7398,6 +7336,68 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
 
   t["default"] = r;
 }, function (e, t, n) {
+  n(85);
+}, function (e, t) {
+  !function () {
+    "use strict";
+
+    var e = 0,
+        t = {},
+        n = {};
+
+    function r(e, t) {
+      return Array.prototype.slice.call((t || document).querySelectorAll(e));
+    }
+
+    function i(e) {
+      if (e.closest) return e.closest("[data-a11y-toggle]");
+
+      for (; e;) {
+        if (1 === e.nodeType && e.hasAttribute("data-a11y-toggle")) return e;
+        e = e.parentNode;
+      }
+
+      return null;
+    }
+
+    function o(e) {
+      var r = e && n[e.getAttribute("aria-controls")];
+      if (!r) return !1;
+      var i = t["#" + r.id],
+          o = "false" === r.getAttribute("aria-hidden");
+      r.setAttribute("aria-hidden", o), i.forEach(function (e) {
+        e.setAttribute("aria-expanded", !o);
+      });
+    }
+
+    var a = function a(i) {
+      t = r("[data-a11y-toggle]", i).reduce(function (e, t) {
+        var n = "#" + t.getAttribute("data-a11y-toggle");
+        return e[n] = e[n] || [], e[n].push(t), e;
+      }, t);
+      var o = Object.keys(t);
+      o.length && r(o).forEach(function (r) {
+        var i = t["#" + r.id],
+            o = r.hasAttribute("data-a11y-toggle-open"),
+            a = [];
+        i.forEach(function (t) {
+          t.id || t.setAttribute("id", "a11y-toggle-" + e++), t.setAttribute("aria-controls", r.id), t.setAttribute("aria-expanded", o), a.push(t.id);
+        }), r.setAttribute("aria-hidden", !o), r.hasAttribute("aria-labelledby") || r.setAttribute("aria-labelledby", a.join(" ")), n[r.id] = r;
+      });
+    };
+
+    document.addEventListener("DOMContentLoaded", function () {
+      a();
+    }), document.addEventListener("click", function (e) {
+      o(i(e.target));
+    }), document.addEventListener("keyup", function (e) {
+      if (13 === e.which || 32 === e.which) {
+        var t = i(e.target);
+        t && "button" === t.getAttribute("role") && o(t);
+      }
+    }), window && (window.a11yToggle = a);
+  }();
+}, function (e, t) {}, function (e, t, n) {
   (function (e) {
     n(88), e(function () {
       e("#slick-menu").slicknav({
