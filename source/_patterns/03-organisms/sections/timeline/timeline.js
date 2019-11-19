49d96c693b05760590c7238eb492bed6eed4f4cc
Marcin Jedras <mjjedras@gmail.com>
2019-11-19 21:48:14 +0100

    Fix timeline controls and multiple timelines

--- source/_patterns/03-organisms/sections/timeline/timeline.js
+++ source/_patterns/03-organisms/sections/timeline/timeline.js
@@ -1,7 +1,7 @@
 require("slick-carousel");
 
-const initControls = () => {
-  $(".slider").on("keydown", e => {
+const initControls = slider => {
+  $(slider).on("keydown", e => {
     switch (e.which) {
       case 37: // left
         $(e.currentTarget).slick("slickPrev");
@@ -15,8 +15,10 @@ const initControls = () => {
     e.preventDefault();
   });
   // Hide slide left on init
-  $(".jcc-timeline__scroller--left").hide();
-  $(".slider").on("afterChange", (e, slick, direction) => {
+  $(slider)
+    .siblings(".jcc-timeline__scroller--left")
+    .hide();
+  $(slider).on("afterChange", (e, slick, direction) => {
     if (direction === 0) {
       $(e.currentTarget)
         .siblings(".jcc-timeline__scroller--left")
@@ -39,39 +41,58 @@ const initControls = () => {
   });
 };
 
-const settings = {
-  infinite: false,
-  slidesToShow: 4,
-  nextArrow: $(".jcc-timeline__scroller--right"),
-  prevArrow: $(".jcc-timeline__scroller--left"),
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 4,
-        dots: false
+const settings = slider => {
+  const defaultSettings = {
+    infinite: false,
+    slidesToShow: 4,
+    nextArrow: $(slider).siblings(".jcc-timeline__scroller--right"),
+    prevArrow: $(slider).siblings(".jcc-timeline__scroller--left"),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 800,
+        arrows: true,
+        settings: {
+          slidesToShow: 3,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: "unslick"
       }
-    },
-    {
-      breakpoint: 800,
-      arrows: true,
-      settings: {
-        slidesToShow: 3,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 640,
-      settings: "unslick"
-    }
-  ]
+    ]
+  };
+  return defaultSettings;
 };
-const slider = $(".slider").slick(settings);
-initControls();
+
+$(".slider").each((_, slider) => {
+  if (slider.children.length > 4) {
+    $(slider).slick(settings(slider));
+    initControls(slider);
+  } else {
+    $(slider)
+      .siblings(".jcc-timeline__scroller--left")
+      .hide();
+    $(slider)
+      .siblings(".jcc-timeline__scroller--right")
+      .hide();
+  }
+});
 
 $(window).on("resize", () => {
-  if ($(window).width() > 640 && !slider.hasClass("slick-initialized")) {
-    $(".slider").slick(settings);
-    initControls();
+  if ($(window).width() > 640) {
+    $(".slider").each((_, slider) => {
+      if (!$(slider).hasClass("slick-initialized") && slider.children.length > 4) {
+        $(slider).slick(settings(slider));
+        initControls(slider);
+      }
+    });
   }
 });
