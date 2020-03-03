18dbf658f44abb2c79cdd30c6f95f5bbf2cac564
Evan Mceldowney <evanmceldowney@gmail.com>
2020-03-02 17:10:45 -0800

    added a colorReplacement option as a third theming option and saved themes to local storage

--- public/js/theming.js
+++ public/js/theming.js
@@ -1,11 +1,97 @@
 var themeSwitcher = document.querySelector(".theme-switcher");
 
+function changeCSS(cssFile, cssLinkIndex) {
+  var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+
+  var newlink = document.createElement("link");
+  newlink.setAttribute("rel", "stylesheet");
+  newlink.setAttribute("type", "text/css");
+  newlink.setAttribute("href", cssFile);
+
+  document
+    .getElementsByTagName("head")
+    .item(0)
+    .replaceChild(newlink, oldlink);
+}
+
+var theme = localStorage["theme"] || "srl";
+
 themeSwitcher.addEventListener("change", function() {
   var themeName = this.value;
   console.log("name: " + themeName);
   document.body.className = "";
   document.body.classList.add(themeName);
+  document.getElementById("trial").disabled = true;
+
+  if (themeName == "trial-court") {
+    document.getElementById("trial").disabled = false;
+    console.log("trial-court switch");
+    localStorage["theme"] = "trial-court";
+  }
+  if (themeName == "srl") {
+    document.getElementById("trial").disabled = true;
+    console.log("srl switch");
+    localStorage["theme"] = "srl";
+  }
+  if (themeName == "trial-court-2") {
+    console.log("trial-court-2 switch");
+    colorReplace("#005ea2", "#000000");
+    localStorage["theme"] = "trial-court-2";
+  }
 });
+
+if (theme == "srl") {
+  document.getElementById("trial").disabled = true;
+  themeSwitcher.value = theme;
+  console.log(theme);
+}
+
+if (theme == "trial-court") {
+  document.getElementById("trial").disabled = false;
+  themeSwitcher.value = theme;
+  console.log(theme);
+}
+
+if (theme == "trial-court-2") {
+  colorReplace("#005ea2", "#000000");
+  document.getElementById("trial").disabled = false;
+  themeSwitcher.value = theme;
+  console.log(theme);
+}
+
+function colorReplace(findHexColor, replaceWith) {
+  // Convert rgb color strings to hex
+  // REF: https://stackoverflow.com/a/3627747/1938889
+  function rgb2hex(rgb) {
+    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+      return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  }
+
+  // Select and run a map function on every tag
+  $("*").map(function(i, el) {
+    // Get the computed styles of each tag
+    var styles = window.getComputedStyle(el);
+
+    // Go through each computed style and search for "color"
+    Object.keys(styles).reduce(function(acc, k) {
+      var name = styles[k];
+      var value = styles.getPropertyValue(name);
+      if (value !== null && name.indexOf("color") >= 0) {
+        // Convert the rgb color to hex and compare with the target color
+        if (value.indexOf("rgb(") >= 0 && rgb2hex(value) === findHexColor) {
+          // Replace the color on this found color attribute
+          $(el).css(name, replaceWith);
+        }
+      }
+    });
+  });
+}
+
+// Call like this for each color attribute you want to replace
 
 /*
 (function() {
