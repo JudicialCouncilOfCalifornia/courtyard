12f593ed5bd8521802e7d1c694813075d832973a
Evan Mceldowney <evanmceldowney@gmail.com>
2020-03-11 12:27:51 -0700

    adding arrows and functionality

---
+++ source/_patterns/02-molecules/navigation/primary-nav/primary-nav.js
@@ -0,0 +1,9 @@
+// require('jquery');
+require("slicknav/jquery.slicknav");
+
+$(function() {
+  $("#slick-menu").slicknav({
+    prependTo: "#js-primary_menu--mobile"
+  });
+  console.log("hellot");
+});
