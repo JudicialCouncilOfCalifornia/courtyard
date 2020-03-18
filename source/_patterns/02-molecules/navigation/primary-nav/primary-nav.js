4f0ce888844cd7e1e759068ee6dc8606a0f4887c
Evan Mceldowney <evanmceldowney@gmail.com>
2020-03-17 18:06:28 -0700

    mobile menu

--- source/_patterns/02-molecules/navigation/primary-nav/primary-nav.js
+++ source/_patterns/02-molecules/navigation/primary-nav/primary-nav.js
@@ -5,18 +5,5 @@ $(function() {
   $("#slick-menu").slicknav({
     prependTo: "#js-header_menu--mobile"
   });
-});
-
-$(document).click(function() {
-  $(".open-nav").hide();
-});
-
-$(".jcc-primary_nav__top_link").click(function() {
-  if ($(this).hasClass("nav-open")) {
-    console.log("hello");
-    $(this).removeClass("nav-open");
-  } else {
-    $(".nav-open").removeClass("nav-open");
-    $(this).addClass("nav-open");
-  }
+  console.log("hello");
 });
