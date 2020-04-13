050923a9665303853f942e2c0542b9da05a965b0
Evan McEldowney <evanmceldowney@gmail.com>
2020-04-13 10:04:24 -0700

    Spl 142 modal color (#237)
    * background fix
    
    * css:

--- source/_patterns/03-organisms/sections/modal/modal.js
+++ source/_patterns/03-organisms/sections/modal/modal.js
@@ -1,4 +1,5 @@
 import A11yDialog from "a11y-dialog";
+import dialogPolyfill from "dialog-polyfill";
 
 // Get the dialog element (with the accessor method you want)
 const el = document.getElementById("jcc-modal");
@@ -6,3 +7,9 @@ const el = document.getElementById("jcc-modal");
 $(".jcc-modal").each(function() {
   new A11yDialog(this);
 });
+
+// var dialog = document.querySelector("dialog");
+// dialogPolyfill.registerDialog(dialog);
+// // Now dialog acts like a native <dialog>.
+// dialog.showModal();
+// console.log("example");
