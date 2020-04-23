c3d138e7fd0761ab90b2bdf544c40bb6bacd3bac
Evan Mceldowney <evanmceldowney@gmail.com>
2020-04-23 16:21:24 -0700

    switchboard next variations

--- source/_patterns/02-molecules/blocks/alert/alert.js
+++ source/_patterns/02-molecules/blocks/alert/alert.js
@@ -1,8 +1,10 @@
-document.getElementById("alert-close").onclick = function alertClose() {
-  var alert = document.getElementById("alert-bar");
-  if (alert.style.display === "none") {
-    alert.style.display = "block";
-  } else {
-    alert.style.display = "none";
-  }
-};
+function alertCloseclick() {
+  document.getElementById("alert-close").onclick = function alertClose() {
+    var alert = document.getElementById("alert-bar");
+    if (alert.style.display === "none") {
+      alert.style.display = "block";
+    } else {
+      alert.style.display = "none";
+    }
+  };
+}
