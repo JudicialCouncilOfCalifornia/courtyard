33bb6f25bd8e0873677a79987261760e50904d52
Evan Mceldowney <evanmceldowney@gmail.com>
2020-04-23 12:23:48 -0700

    inline updates

---
+++ source/_patterns/02-molecules/blocks/alert/alert.js
@@ -0,0 +1,8 @@
+document.getElementById("alert-close").onclick = function alertClose() {
+  var alert = document.getElementById("alert-bar");
+  if (alert.style.display === "none") {
+    alert.style.display = "block";
+  } else {
+    alert.style.display = "none";
+  }
+};
