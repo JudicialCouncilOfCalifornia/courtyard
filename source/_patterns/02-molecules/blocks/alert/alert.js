cd6f6cb909e09a43788e5d42926786dfea185a84
Jesse James Arnold <jesse.james@exygy.com>
2020-05-18 14:15:52 -0700

    Case bugs (#307)
    * remove complete from case page
    
    * fixing dual timeline bugs
    
    * fixing sass formatting and element order
    
    * Fix for alert
    
    Co-authored-by: Jesse Arnold <jessearnold@Jesses-MacBook-Pro.local>
    Co-authored-by: Marcin Jedras <mjjedras@gmail.com>

--- source/_patterns/02-molecules/blocks/alert/alert.js
+++ source/_patterns/02-molecules/blocks/alert/alert.js
@@ -1,17 +1,18 @@
-let messageContentObject = document.getElementsByClassName("usa-alert__text"),  cookieId;
-if(messageContentObject[0]) {
+let messageContentObject = document.getElementsByClassName("usa-alert__text"),
+  cookieId;
+if (messageContentObject[0]) {
   cookieId = "patternlab-alert-hide-" + stringToHash(messageContentObject[0].innerText);
 }
 var alert = document.getElementById("alert-bar");
 
-if(alert) {
+if (alert) {
   // Message loads hidden to avoid flash if cookie is found.
   if (!getCookie(cookieId)) {
     alert.classList.add("active");
   }
 
   document.getElementById("alert-close").onclick = function alertClose() {
-    if (alert.classList.contains('active')) {
+    if (alert.classList.contains("active")) {
       setCookie(cookieId, true, 7);
       alert.classList.remove("active");
     }
@@ -19,29 +20,29 @@ if(alert) {
 }
 
 // Helper functions
-function setCookie(name,value,days) {
+function setCookie(name, value, days) {
   var expires = "";
   if (days) {
     var date = new Date();
-    date.setTime(date.getTime() + (days*24*60*60*1000));
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = "; expires=" + date.toUTCString();
   }
-  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 function getCookie(name) {
   var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
   }
   return null;
 }
 
 // In case we add some logic
 function eraseCookie(name) {
-  document.cookie = name+'=; Max-Age=-99999999;';
+  document.cookie = name + "=; Max-Age=-99999999;";
 }
 
 function stringToHash(string) {
@@ -49,7 +50,7 @@ function stringToHash(string) {
   if (string.length == 0) return hash;
   for (let i = 0; i < string.length; i++) {
     let char = string.charCodeAt(i);
-    hash = ((hash << 5) - hash) + char;
+    hash = (hash << 5) - hash + char;
     hash = hash & hash;
   }
   return hash;
