a08be0ac35a703f7e4e831d7a1b776d894ef50a6
Mel Wong <46764557+melwong-jcc@users.noreply.github.com>
2020-06-04 16:31:05 -0700

    Feature/tc 244  search translate accessibility (#339)
    * TC-244: improved on click and focus event handling foundation
    
    * TC-244: improved on event handling foundation
    
    * TC-244: missing tab behaviors that match slicknav & general fine-tuning

--- source/_patterns/03-organisms/global/header-trial/header-trial.js
+++ source/_patterns/03-organisms/global/header-trial/header-trial.js
@@ -1,19 +1,58 @@
 require("slicknav/jquery.slicknav");
 
-$(".mobile_button").click(function() {
+var mobileMenuClass = ".mobile_button";
+
+// toggle mobile menus/functions closed as needed
+function resetMenus(includeMain) {
+  $(".show-button").removeClass("active-menu");
   $(".show-button").hide();
+  if (includeMain) {
+    $("#slick-menu").slicknav("close");
+  }
+}
+
+// Event handling for menus/functions
+$(mobileMenuClass).on("click keydown", function(e) {
+  var mobileMenuID = $(this).attr("id");
+  var isMobileMenu = $("#" + mobileMenuID).hasClass("jcc-primary-nav");
+  if (!isMobileMenu) {
+    var toggleMenu = function() {
+      if ($("#show-" + mobileMenuID).hasClass("active-menu")) {
+        resetMenus(true);
+      } else {
+        resetMenus(true);
+        $("#show-" + mobileMenuID)
+          .toggle()
+          .addClass("active-menu");
+      }
+    };
+
+    switch (e.type) {
+      case "click":
+        toggleMenu();
+        break;
+      case "keydown":
+        switch (e.keyCode) {
+          case 13: //enter
+            toggleMenu();
+        }
+    }
+  } else {
+    resetMenus(false);
+  }
 });
 
-$("#search").click(function() {
-  $("#show-search").show();
-  $("#slick-menu").slicknav("close");
-});
-
-$("#translate").click(function() {
-  $("#show-dropbutton").toggle();
-  $("#slick-menu").slicknav("close");
+// Close menus/functions if click focus is away
+$(document).on("click", function(e) {
+  if (!$(mobileMenuClass).is(":focus") && !$(".slicknav_open").is(":focus")) {
+    resetMenus(true);
+  }
 });
 
 $(function() {
   $("#js-header-trial_menu--mobile").append($("#js-header_menu--mobile"));
+  $(".slicknav_btn").attr(
+    "aria-label",
+    "Main navigation menu. Open or close menu by using ENTER key now."
+  );
 });
