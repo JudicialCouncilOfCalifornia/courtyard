7bf7b9749db336c0b107c87a983dc6e9326298f1
Mel Wong <mel.wong@jud.ca.gov>
2020-10-01 16:40:45 -0700

    SRL-282: adjustments/corrections for mobile use

--- source/_patterns/02-molecules/blocks/share/share.js
+++ source/_patterns/02-molecules/blocks/share/share.js
@@ -1,11 +1,10 @@
 require("a11y-toggle");
 
 $(".jcc-share__item").on("click tap", function() {
-  console.log("Close any open modals");
   $(".jcc-share__content").attr("aria-hidden", "true");
 });
 
-$("#print").on("click tap", function() {
+$("#print, #print-mobile").on("click tap", function() {
   window.print();
   return false;
 });
