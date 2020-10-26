9fbc33392c0de1f94711eb777ebdd670820ec087
Rob Decker <rob@deckerdev.com>
2020-10-26 15:11:41 -0700

    SRL-317 Cascading jump menu

--- source/_patterns/02-molecules/navigation/cascading-jump-menu/cascading-jump-menu.js
+++ source/_patterns/02-molecules/navigation/cascading-jump-menu/cascading-jump-menu.js
@@ -7,21 +7,26 @@ $(".jcc-cascading-jump-menu").each(function() {
     .val();
   data = JSON.parse(unescape(data));
 
+  // Default 'child' option to use when no 'parent' option selected
+  const child_default_option = "<option value=''>" + data.default_child_option + "</option>";
+
   $parent.on("change", function() {
-    const group = $(this).val();
+    const groupName = $(this).val();
     let options = "";
-    if (group) {
-      const groupItems = data[group].items;
-      const child_default_option = "<option value=''>" + data[group].default_option + "</option>";
+    if (groupName) {
+      const groupItems = data.groups[groupName].items;
+      const default_option =
+        "<option value=''>" + data.groups[groupName].default_option + "</option>";
 
       // Update 'child' select options
       options = $.map(groupItems, function(groupItem) {
         return '<option value="' + groupItem.url + '">' + groupItem.name + "</option>";
       }).join("");
 
-      $child.html(child_default_option + options);
+      $child.html(default_option + options);
     } else {
-      $child.html("");
+      // Reset 'child' options
+      $child.html(child_default_option);
     }
 
     if (options.length > 0) {
@@ -33,7 +38,7 @@ $(".jcc-cascading-jump-menu").each(function() {
 });
 
 $(".jcc-cascading-jump-menu button").on("click", function() {
-  // Get url from child
+  // Get url from 'child'
   const $child = $(this)
     .parents(".jcc-cascading-jump-menu")
     .find(".jcc-cascading-jump-menu__child");
