cf65f5e2b21c84708d35bb65bd8ba176aacf59af
Mel Wong <46764557+melwong-jcc@users.noreply.github.com>
2020-11-16 15:53:46 -0800

    Hotfix/srl 349  suppress themeswitcher (#464)
    * SRL-349: suppress themeswitcher outside of lib & modal improvements
    
    * SRL-349: processed CSS & JS

--- source/js/theming.js
+++ source/js/theming.js
@@ -1,51 +1,54 @@
 var themeSwitcher = $(".jcc-themebar__switcher");
-var theme = localStorage["theme"] || "srl";
 
-var toggle = function(selector, display) {
-  $(selector).each(function(idx, element) {
-    if (display) {
-      element.style.display = display;
-    } else {
-      element.style.display = element.style.display == "none" ? "block" : "none";
-    }
-  });
-};
+if (themeSwitcher.length > 0) {
+  var theme = localStorage["theme"] || "srl";
 
-if (themeSwitcher) {
-  $(themeSwitcher).change(function() {
-    var themeName = this.value;
-    console.log("name: " + themeName);
-    document.body.className = "";
-    document.body.classList.add(themeName);
-    document.getElementById("trial").disabled = true;
+  var toggle = function(selector, display) {
+    $(selector).each(function(idx, element) {
+      if (display) {
+        element.style.display = display;
+      } else {
+        element.style.display = element.style.display == "none" ? "block" : "none";
+      }
+    });
+  };
 
-    if (themeName == "trial-court") {
-      document.getElementById("trial").disabled = false;
-      console.log("trial-court switch");
-      toggle("sg-colors-trial", "block");
-      toggle("sg-colors-srl", "none");
-      localStorage["theme"] = "trial-court";
-    }
-    if (themeName == "srl") {
+  if (themeSwitcher) {
+    $(themeSwitcher).change(function() {
+      var themeName = this.value;
+      console.log("name: " + themeName);
+      document.body.className = "";
+      document.body.classList.add(themeName);
       document.getElementById("trial").disabled = true;
-      console.log("srl switch");
+
+      if (themeName == "trial-court") {
+        document.getElementById("trial").disabled = false;
+        console.log("trial-court switch");
+        toggle("sg-colors-trial", "block");
+        toggle("sg-colors-srl", "none");
+        localStorage["theme"] = "trial-court";
+      }
+      if (themeName == "srl") {
+        document.getElementById("trial").disabled = true;
+        console.log("srl switch");
+        toggle("sg-colors-trial", "none");
+        toggle("sg-colors-srl", "block");
+        localStorage["theme"] = "srl";
+      }
+    });
+
+    if (theme == "srl") {
+      document.getElementById("trial").disabled = true;
       toggle("sg-colors-trial", "none");
       toggle("sg-colors-srl", "block");
-      localStorage["theme"] = "srl";
+      themeSwitcher.value = theme;
     }
-  });
 
-  if (theme == "srl") {
-    document.getElementById("trial").disabled = true;
-    toggle("sg-colors-trial", "none");
-    toggle("sg-colors-srl", "block");
-    themeSwitcher.value = theme;
-  }
-
-  if (theme == "trial-court") {
-    document.getElementById("trial").disabled = false;
-    toggle("sg-colors-trial", "block");
-    toggle("sg-colors-srl", "none");
-    themeSwitcher.value = theme;
+    if (theme == "trial-court") {
+      document.getElementById("trial").disabled = false;
+      toggle("sg-colors-trial", "block");
+      toggle("sg-colors-srl", "none");
+      themeSwitcher.value = theme;
+    }
   }
 }
