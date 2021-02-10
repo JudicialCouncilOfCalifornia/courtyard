fac1353a0c0ade8f09d80859af9b7e969422962b
Mel Wong <46764557+melwong-jcc@users.noreply.github.com>
2021-02-09 18:07:09 -0800

    WSGEN-1223: addressed parallax null exception (#513)

--- source/_patterns/02-molecules/blocks/hero/hero.js
+++ source/_patterns/02-molecules/blocks/hero/hero.js
@@ -1,5 +1,8 @@
-window.addEventListener("scroll", function(e) {
-  var scrolled = window.pageYOffset;
-  var heroParallax = document.querySelector(".jcc-hero--background-parallax img");
-  heroParallax.style.top = -(scrolled * 0.4) + "px";
-});
+var heroParallax = document.querySelector(".jcc-hero--background-parallax img");
+
+if (heroParallax) {
+  window.addEventListener("scroll", function(e) {
+    var scrolled = window.pageYOffset;
+    heroParallax.style.top = -(scrolled * 0.4) + "px";
+  });
+}
