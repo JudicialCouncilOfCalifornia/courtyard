b3608d8f4126fc1069dd60979ae65e95302f1026
Mel Wong <mel.wong@jud.ca.gov>
2021-06-28 12:11:39 -0700

    minification already provided through prod build

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,6 @@ const strip = require("gulp-strip-comments");
 const svgmin = require("gulp-svgmin");
 const svgstore = require("gulp-svgstore");
 const cheerio = require("gulp-cheerio");
-const cleanCss = require("gulp-clean-css");
 
 const pkg = require("./node_modules/uswds/package.json");
 const uswds = "./node_modules/uswds/dist";
@@ -151,22 +150,8 @@ const buildCss = (source, includes, destination) => {
   return css.pipe(sourcemaps.write("./")).pipe(gulp.dest(destination)).pipe(browserSync.stream());
 };
 
-const minifyCss = (destination) => {
-  let css = gulp
-    .src([destination + "/*.css"])
-    .pipe(cleanCss({ compatibility: "*"}))
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest(destination));
-
-  return css;
-}
-
 const plCss = () => {
   return buildCss(config.css.src, config.css.project_scss, config.css.public_folder);
-};
-
-const minifyPlCss = () => {
-  return minifyCss(config.css.public_folder);
 };
 
 const styleguideCss = () => {
@@ -252,7 +237,6 @@ const fullBuild = gulp.series(
   copyUswdsImages,
   buildIcons,
   plCss,
-  minifyPlCss,
   plJs,
   styleguideCss,
   styleguideJs
@@ -265,7 +249,6 @@ const partialBuild = gulp.series(
   copyUswdsImages,
   buildIcons,
   plCss,
-  minifyPlCss,
   plJs
 );
 
