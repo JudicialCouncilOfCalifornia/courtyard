f7aa847b034adde500ba936f80cd25c40cd681fe
Mel Wong <46764557+melwong-jcc@users.noreply.github.com>
2021-08-06 14:29:29 -0700

    Feature/sc 37  header pro features (#596)
    * SC-37: pro theme setup plus masthead updates
    
    * SC-37: missed updated files during git add
    
    * SC-37: removed blue line in translate menu

--- source/_patterns/02-molecules/video/video.js
+++ source/_patterns/02-molecules/video/video.js
@@ -1,13 +1,13 @@
-document.querySelectorAll('.jcc-video .jcc-video__cover').forEach(item => {
-  item.addEventListener('click', event => {
+document.querySelectorAll(".jcc-video .jcc-video__cover").forEach((item) => {
+  item.addEventListener("click", (event) => {
     event.preventDefault();
 
-    const wrapper = event.target.closest('.jcc-video');
-    const iframe = wrapper.querySelector('iframe:first-child');
-    const src = iframe.getAttribute('data-src');
+    const wrapper = event.target.closest(".jcc-video");
+    const iframe = wrapper.querySelector("iframe:first-child");
+    const src = iframe.getAttribute("data-src");
 
     // Hide the cover & set the src so the video will start playing
-    wrapper.classList.add('video-active');
-    iframe.setAttribute('src', src);
-  })
+    wrapper.classList.add("video-active");
+    iframe.setAttribute("src", src);
+  });
 });
