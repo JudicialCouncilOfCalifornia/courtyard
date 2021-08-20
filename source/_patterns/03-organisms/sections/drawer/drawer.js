fbeeac69f79a3b48cd5b730ac9d4a4a0cdee074c
Mel Wong <mel.wong@jud.ca.gov>
2021-08-19 20:16:23 -0700

    SRL-443: displat at 50% scroll and sticky footer when mobile

--- source/_patterns/03-organisms/sections/drawer/drawer.js
+++ source/_patterns/03-organisms/sections/drawer/drawer.js
@@ -1,16 +1,11 @@
 // Elements.
 const $window = $(window);
+const $body = document.body;
 const $feedback_trigger = $('[data-feedback^="trigger"]');
 const $feedback_container = $('[data-feedback="container"]');
 const $drawer = $(".jcc-drawer");
 const $feedback_dialog = $('[data-feedback="dialog"]');
-
-const isScrolledToBottom = ($offset = 0) => {
-  // Bottom is the bottom of the drawer's previous sibling.
-  const $bottom = $drawer.prev().offset().top + $drawer.prev().height();
-  // Scroll past the bottom to the offset, if any.
-  return $window.scrollTop() + $window.height() - $offset >= $bottom;
-};
+const $footer_spacer = $(".usa-footer .jcc-global-bar");
 
 const pageIsShorterThanWindow = ($offset = 0) => {
   return $(document).height() - $offset <= $window.height();
@@ -27,33 +22,51 @@ const isNoPageScroll = () => {
   return docHeight == scroll;
 };
 
+function siblingCheck() {
+  if ($(window).width() <= 1024 && $feedback_container.siblings().length == 0) {
+    $feedback_container.attr("style", "width: 100%");
+  } else {
+    $feedback_container.removeAttr("style");
+  }
+}
+
 // Initial visibility.
 $(document).ready(function() {
+  if ($(window).width() <= 1024) {
+    siblingCheck();
+  }
+
   if (isNoPageScroll() == true) {
     $drawer.attr("visible", "visible");
   }
 });
 
 // Scroll.
-$window.on("scroll", function() {
-  if ($drawer.length == 0) {
-    return;
-  }
-
-  if ((isScrolledToBottom($drawer.height() / 2) && isSmallScreen()) || isSmallScreen() == false) {
-    $drawer.attr("visible", "visible");
-  } else {
-    if (!$feedback_dialog.attr("open")) {
-      $drawer.removeAttr("visible");
+if (!isNoPageScroll()) {
+  $window.on("scroll", function() {
+    if ($drawer.length == 0) {
+      return;
     }
-  }
-});
 
-$(window).resize(function() {
-  if ($feedback_dialog.attr("open")) {
-    $drawer.attr("visible", "visible");
-  }
-});
+    if ($window.scrollTop() + $window.height() >= $body.scrollHeight / 2) {
+      $drawer.attr("visible", "visible");
+    }
+  });
+
+  $(window).resize(function() {
+    if ($feedback_dialog.attr("open")) {
+      $drawer.attr("visible", "visible");
+    }
+
+    if ($(window).width() <= 1024) {
+      siblingCheck();
+      $footer_spacer.attr("style", "padding-bottom: 7rem");
+    } else {
+      siblingCheck();
+      $footer_spacer.removeAttr("style");
+    }
+  });
+}
 
 // Widget interaction/visibility.
 $feedback_trigger.on("click", function(e) {
